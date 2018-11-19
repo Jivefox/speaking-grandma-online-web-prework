680cf20102926bf2edcae452bcb86f1e4286d63e
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-19 07:03:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+  else phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
   end
 end
